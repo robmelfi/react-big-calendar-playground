e6506e39e38949ea331c9a2d1c30517b7276ff0b
robmelfi <melfiroberto@gmail.com>
2018-10-31 11:08:41 +0100

    Custom Toolbar - convert in react component

--- src/CustomToolbar/CustomToolbar.js
+++ src/CustomToolbar/CustomToolbar.js
@@ -11,81 +11,82 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 // to the correct localizer.
 const localizer = BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
-const CustomToolbar = props => {
+class CustomToolbar extends React.Component {
 
-    let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
+    render() {
+        let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
-    const CustomToolbar = (toolbar) => {
+        const CustomToolbar = (toolbar) => {
 
-        const goToBack = () => {
-            toolbar.onNavigate(BigCalendar.Navigate.PREVIOUS);
-        };
+            const goToBack = () => {
+                toolbar.onNavigate(BigCalendar.Navigate.PREVIOUS);
+            };
 
-        const goToNext = () => {
-            toolbar.onNavigate(BigCalendar.Navigate.NEXT);
-        };
+            const goToNext = () => {
+                toolbar.onNavigate(BigCalendar.Navigate.NEXT);
+            };
 
-        const goToCurrent = () => {
-            toolbar.onNavigate(BigCalendar.Navigate.TODAY);
-        };
+            const goToCurrent = () => {
+                toolbar.onNavigate(BigCalendar.Navigate.TODAY);
+            };
 
-        const goToWeekView = () => {
-            toolbar.onView(BigCalendar.Views.WEEK)
-        };
+            const goToWeekView = () => {
+                toolbar.onView(BigCalendar.Views.WEEK)
+            };
 
-        const goToMonthView = () => {
-            toolbar.onView(BigCalendar.Views.MONTH)
-        };
+            const goToMonthView = () => {
+                toolbar.onView(BigCalendar.Views.MONTH)
+            };
 
-        const goToDayView = () => {
-            toolbar.onView(BigCalendar.Views.DAY)
-        };
+            const goToDayView = () => {
+                toolbar.onView(BigCalendar.Views.DAY)
+            };
 
-        const label = () => {
-            console.log();
-            return (
-                <span>{toolbar.label}</span>
+            const label = () => {
+                console.log();
+                return (
+                    <span>{toolbar.label}</span>
+                );
+            };
+
+            return(
+                <div className="mb-3 d-flex justify-content-between">
+                    <div className="d-flex flex-column align-items-center">
+                        <ButtonGroup size="sm">
+                            <Button outline color="primary" onClick={goToBack}>&#8249;</Button>
+                            <Button outline color="primary" onClick={goToCurrent}>today</Button>
+                            <Button outline color="primary" onClick={goToNext}>&#8250;</Button>
+                        </ButtonGroup>
+                    </div>
+                    <div className="d-flex flex-row align-items-center text-primary lead">
+                        {label()}
+                    </div>
+                    <div className="d-flex flex-column justify-content-end">
+                        <ButtonGroup size="sm">
+                            <Button outline color="primary" onClick={goToMonthView}>Month</Button>
+                            <Button outline color="primary" onClick={goToWeekView}>Week</Button>
+                            <Button outline color="primary" onClick={goToDayView}>Day</Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
             );
         };
 
         return (
-            <div className="mb-3 d-flex justify-content-between">
-                <div className="d-flex flex-column align-items-center">
-                    <ButtonGroup size="sm">
-                        <Button outline color="primary" onClick={goToBack}>&#8249;</Button>
-                        <Button outline color="primary" onClick={goToCurrent}>today</Button>
-                        <Button outline color="primary" onClick={goToNext}>&#8250;</Button>
-                    </ButtonGroup>
-                </div>
-                <div className="d-flex flex-row align-items-center text-primary">
-                    {label()}
-                </div>
-                <div className="d-flex flex-column justify-content-end">
-                    <ButtonGroup size="sm">
-                        <Button outline color="primary" onClick={goToMonthView}>Month</Button>
-                        <Button outline color="primary" onClick={goToWeekView}>Week</Button>
-                        <Button outline color="primary" onClick={goToDayView}>Day</Button>
-                    </ButtonGroup>
-                </div>
-            </div>
+            <Container className="calendar">
+                <BigCalendar
+                    events={events}
+                    step={60}
+                    showMultiDayTimes
+                    defaultDate={new Date(2015, 3, 1)}
+                    localizer={localizer}
+                    components={{
+                        toolbar: CustomToolbar
+                    }}
+                />
+            </Container>
         );
     };
-
-    return (
-        <Container className="calendar">
-            <BigCalendar
-                events={events}
-                step={60}
-                showMultiDayTimes
-                defaultDate={new Date(2015, 3, 1)}
-                localizer={localizer}
-                components={{
-                    toolbar: CustomToolbar
-                }}
-            />
-        </Container>
-
-    );
-};
+}
 
 export default CustomToolbar;
